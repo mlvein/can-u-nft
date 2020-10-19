6574ceac1526035d3b9a6266c176f6670020459e
Melvin Alvarez <alvarezmelvin@me.com>
2020-10-19 22:19:07 +0800

    init style
    - initial card style
    - responsive minmax needs refinement
    - collating content for NFT info

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
+var resultPossible = 
+    `
+    <p>Yes, It's possible! Here are some popular platforms that have done so:</p>
+        <ul>
+            <li>makersplace</li>
+            <li>rarible</li>
+        </ul>
+    `;
+
 function search() {
     var searchResult;
     var searchQuery = document.getElementById("input001").value;
+    // searchQuery = searchQuery.toLowerCase;
+    
     
     switch (searchQuery) {
         case "chicken":
             searchResult = "Hmm... it might be possible!";
-            break;
-        case "egg":
-            searchResult = "Yes! It's possible";
-            break;
+        break;
+        case "art":
+        case "music":
+            searchResult = resultPossible;
+        break;
+        case "cats":
+            searchResult = "Well... sort of";
+        break;
         default:
             searchResult = "Not sure...";
-            break;
+        break;
     }
+
     document.getElementById("result").innerHTML = searchResult;
   }
